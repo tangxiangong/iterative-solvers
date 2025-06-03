decd0ea6a853ee888661288fc633fddef657e4bd
tangxiangong <tangxiangong@gmail.com>
2025-06-03 20:01:13 +0800

    feat: add modules  `errors` and `solver`

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 #![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
 
-extern crate nalgebra as na;
-
 pub mod cg;
 pub use cg::*;
 
 pub mod utils;
 pub use utils::*;
+
+pub mod error;
+pub use error::*;
+
+mod solver;
+pub use solver::*;
