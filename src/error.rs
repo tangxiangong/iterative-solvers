25afcebb3ee4470ddead6a383e3983e14034ba38
tangxiangong <tangxiangong@gmail.com>
2025-06-04 09:36:55 +0800

    feat: Increase the error types and result types of the iterative solver, including descriptions of dimension matching errors.

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,14 @@
+//! Error and Result type for iterative solvers.
+
 use thiserror::Error;
 
+/// Error type for iterative solvers.
 #[derive(Error, Debug, Clone, PartialEq, Eq, Ord, PartialOrd)]
 pub enum IterSolverError {
+    /// Dimension Match Error of Matrices and Vectors.
     #[error("Dimension Not Match: {0}")]
     DimensionError(String),
 }
 
+/// Result type for iterative solvers.
 pub type IterSolverResult<T> = Result<T, IterSolverError>;
