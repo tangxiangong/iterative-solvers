402c39b9a5bd18cafe46cb91476f865ffbcaeeda
tangxiangong <tangxiangong@gmail.com>
2025-06-10 17:56:19 +0800

    feat: In the `IterSolverError` enumeration, add the `InvalidInput` error type to handle invalid input

--- src/error.rs
+++ src/error.rs
@@ -8,6 +8,10 @@ pub enum IterSolverError {
     /// Dimension Match Error of Matrices and Vectors.
     #[error("Dimension Not Match: {0}")]
     DimensionError(String),
+
+    /// Invalid Input.
+    #[error("Invalid Input: {0}")]
+    InvalidInput(String),
 }
 
 /// Result type for iterative solvers.
