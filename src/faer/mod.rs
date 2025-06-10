5640e739829efd092d9e3960b452174cf150b931
tangxiangong <tangxiangong@gmail.com>
2025-06-10 17:58:45 +0800

    feat: Add tool module, including related functions for sparse and dense matrices, and introduce new utility functions in the `faer` module.

--- src/faer/mod.rs
+++ src/faer/mod.rs
@@ -1,1 +1,10 @@
 pub mod cg;
+pub use cg::*;
+pub mod ops;
+pub use ops::*;
+
+pub mod utils;
+
+pub(crate) fn is_vector(mat: &faer::Mat<f64>) -> bool {
+    mat.ncols() == 1
+}
---
+++ src/faer/utils/mod.rs
@@ -0,0 +1,4 @@
+//! Utility functions for creating sparse and dense matrices.
+
+pub mod dense;
+pub mod sparse;
