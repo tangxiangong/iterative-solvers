418c53a87c7df5c0566c5263a25f7bc2f316f1c7
tangxiangong <tangxiangong@gmail.com>
2025-06-12 00:11:19 +0800

    feat: Implement the gemv operation for sparse matrices, optimize the matrix multiplication function, and remove unnecessary dot and axpy methods.

--- src/faer/ops.rs
+++ src/faer/ops.rs
@@ -1,10 +1,9 @@
 use faer::{
-    Conj, Mat,
-    linalg::matmul::{dot::inner_prod, matmul},
+    Mat,
+    linalg::matmul::matmul,
     matrix_free::LinOp,
+    sparse::{SparseColMat, SparseRowMat, linalg::matmul::sparse_dense_matmul},
 };
-
-use crate::{IterSolverError, IterSolverResult, is_vector};
 
 pub trait MatOp: LinOp<f64> {
     fn is_square(&self) -> bool;
@@ -14,11 +13,6 @@ pub trait MatOp: LinOp<f64> {
     fn is_empty(&self) -> bool {
         self.len() == 0
     }
-
-    fn dot(&self, rhs: &Mat<f64>) -> IterSolverResult<f64>;
-
-    /// self = alpha * x + beta * self
-    fn axpy(&mut self, alpha: f64, x: &Mat<f64>, beta: f64);
 
     /// y = alpha * self * x + beta * y
     fn gemv(&self, alpha: f64, x: &Mat<f64>, beta: f64, y: &mut Mat<f64>);
@@ -33,39 +27,56 @@ impl MatOp for Mat<f64> {
         self.nrows() * self.ncols()
     }
 
-    fn dot(&self, rhs: &Mat<f64>) -> IterSolverResult<f64> {
-        if !is_vector(self) {
-            return Err(IterSolverError::InvalidInput(
-                "The input parameter is not a vector".to_string(),
-            ));
-        }
-        if !is_vector(rhs) {
-            return Err(IterSolverError::InvalidInput(
-                "The input parameter is not a vector".to_string(),
-            ));
-        }
-        if self.nrows() != rhs.nrows() {
-            return Err(IterSolverError::InvalidInput(
-                "The input parameter is not a vector".to_string(),
-            ));
-        }
-        Ok(inner_prod(
-            self.row(0),
-            Conj::No,
-            rhs.row(0).transpose(),
-            Conj::No,
-        ))
+    /// y = alpha * self * x + beta * y
+    fn gemv(&self, alpha: f64, x: &Mat<f64>, beta: f64, y: &mut Mat<f64>) {
+        *y *= beta;
+        matmul(y, faer::Accum::Add, self, x, alpha, faer::Par::Seq);
+    }
+}
+
+impl MatOp for SparseRowMat<usize, f64> {
+    fn is_square(&self) -> bool {
+        self.nrows() == self.ncols()
     }
 
-    /// self = alpha * x + beta * self
-    fn axpy(&mut self, alpha: f64, x: &Mat<f64>, beta: f64) {
-        *self *= beta;
-        *self = alpha * x + &*self;
+    fn len(&self) -> usize {
+        self.nrows() * self.ncols()
     }
 
     /// y = alpha * self * x + beta * y
     fn gemv(&self, alpha: f64, x: &Mat<f64>, beta: f64, y: &mut Mat<f64>) {
         *y *= beta;
-        matmul(y, faer::Accum::Add, self, x, alpha, faer::Par::Seq);
+        let col_mat = self.to_col_major().unwrap();
+        sparse_dense_matmul(
+            y.as_mut(),
+            faer::Accum::Add,
+            col_mat.as_ref(),
+            x.as_ref(),
+            alpha,
+            faer::Par::Seq,
+        );
+    }
+}
+
+impl MatOp for SparseColMat<usize, f64> {
+    fn is_square(&self) -> bool {
+        self.nrows() == self.ncols()
+    }
+
+    fn len(&self) -> usize {
+        self.nrows() * self.ncols()
+    }
+
+    /// y = alpha * self * x + beta * y
+    fn gemv(&self, alpha: f64, x: &Mat<f64>, beta: f64, y: &mut Mat<f64>) {
+        *y *= beta;
+        sparse_dense_matmul(
+            y.as_mut(),
+            faer::Accum::Add,
+            self.as_ref(),
+            x.as_ref(),
+            alpha,
+            faer::Par::Seq,
+        );
     }
 }
