84ef38395888ea4188531c88d6e995e64ea1e75e
tangxiangong <tangxiangong@gmail.com>
2025-07-10 10:15:10 +0800

    bench: switch to sparse matrix representation in CG benchmark
    - Updated the matrix creation in the Conjugate Gradient benchmark to use `spdiagm` for improved memory efficiency.
    - Added `SparseArrays` to the imports to support the new matrix type.

--- julia-bench/src/cg.jl
+++ julia-bench/src/cg.jl
@@ -1,9 +1,9 @@
-using IterativeSolvers, BenchmarkTools, LinearAlgebra
+using IterativeSolvers, BenchmarkTools, LinearAlgebra, SparseArrays
 
 N = 1024
-h = 1/N
+h = 1 / N
 
-A = diagm(0=>2/h^2 * ones(N-1), 1=>-1/h^2 * ones(N-2), -1=>-1/h^2 * ones(N-2))
+A = spdiagm(0 => 2 / h^2 * ones(N - 1), 1 => -1 / h^2 * ones(N - 2), -1 => -1 / h^2 * ones(N - 2))
 b = π^2 * sin.(π * (1:N-1) * h)
 abstol = 1e-10
 reltol = 1e-8
